f3ec1e6e044b0f81328bb485b0a4521dc40fe4ea
puneethcm <puneethkumarcm8050@gmail.com>
2023-02-18 14:50:00 +0530

    First Name Vlaidation

--- UserInputValidation/Program.cs
+++ UserInputValidation/Program.cs
@@ -1,3 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using UserInputValidation;
 
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to User Input Validation");
+        Console.WriteLine("Enter First Name");
+        string firstName = Console.ReadLine();
+        UserValidation validate = new UserValidation(firstName);
+        validate.ValidateFirstName();
+        Console.ReadLine();
+    }
+}
---
+++ UserInputValidation/UserValidation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace UserInputValidation
+{
+	public class UserValidation
+	{
+		public const string First_Name = "^[A-Z]{1}[a-zA-Z]{3,}$";
+		public string first;
+
+		public UserValidation(string firstName)
+		{
+			this.first = firstName;
+		}
+
+		public void ValidateFirstName()
+		{
+			Regex regex = new Regex(First_Name);
+			bool result = regex.IsMatch(this.first);
+
+			if (result == true)
+			{
+				Console.WriteLine("The Enter First Name is Valid");
+			}
+			else
+			{
+                Console.WriteLine("The Enter First Name is Not Valid");
+				Console.WriteLine("The First letter shoud be capital");
+            }
+		}
+    }
+}
+
